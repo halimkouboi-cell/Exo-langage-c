10bfd60458c0ae202a2a20a41d8f93965f3b1457
halimkouboi-cell <halimkouboi@gmail.com>
2026-01-28 10:18:25 +0100

    la moyenne des entiers paires

---
+++ Documents/genie logiciel L1/LANGAGE C/exercice/exo3/exo moi/EXO BOUCLE/recorrection/versioning/exo2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+int m,n,moy,i,s=0,cpt=0,CPT=0;
+do{
+    printf("entrez le nombre d'entier: ");
+    scanf("%d",&n);
+    if(n<=0){
+    puts("entier invalide");
+    }
+}while(n<=0);
+for(i=0;i<n;i++){
+        do{
+    puts("veuillez saisir un entier: ");
+    scanf("%d", &m);
+    }while(m<=0);
+    if(m%2==0){
+        cpt++;
+        s+=m;
+    }
+    }
+
+moy=s/cpt;
+printf("la moyenne des entiers pairs est %d \n",moy);
+
+return 0;}
